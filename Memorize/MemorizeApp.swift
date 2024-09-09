ed93c5078692644ca98a5f5c591d2f5c7f2a7e9f
samialhamad <40968198+samialhamad@users.noreply.github.com>
2024-09-09 15:27:13 -0700

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Sami Alhamad on 9/9/24.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
