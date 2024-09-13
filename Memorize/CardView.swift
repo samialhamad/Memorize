155dc036df1d9ab9ddad5b01bbe0f18b252d68a1
samialhamad <40968198+samialhamad@users.noreply.github.com>
2024-09-13 12:27:55 -0700

    Initial commit

---
+++ Memorize/CardView.swift
@@ -0,0 +1,81 @@
+//
+//  CardView.swift
+//  Memorize
+//
+//  Created by Sami Alhamad on 9/12/24.
+//
+
+import SwiftUI
+
+struct CardView: View {
+    typealias Card = MemoryGame<String>.Card
+    
+    let card: Card
+    
+    init(_ card: Card) {
+        self.card = card
+    }
+    
+    var body: some View {
+        TimelineView(.animation) { timeline in
+            if card.isFaceUp || !card.isMatched {
+                Pie(endAngle: .degrees(card.bonusPercentRemaining * 360))
+                    .opacity(Constants.Pie.opacity)
+                    .overlay(cardsContents.padding(Constants.Pie.inset))
+                    .padding(Constants.inset)
+                    .cardify(isFaceUp: card.isFaceUp)
+                    .transition(.scale)
+            } else {
+                Color.clear
+            }
+        }
+    }
+    
+    var cardsContents: some View {
+        Text(card.content)
+            .font(.system(size: Constants.FontSize.largest))
+            .minimumScaleFactor(Constants.FontSize.scaleFactor)
+            .multilineTextAlignment(.center)
+            .aspectRatio(1, contentMode: .fit)
+            .rotationEffect(.degrees(card.isMatched ? 360 : 0))
+            .animation(.spin(duration:1), value: card.isMatched)
+    }
+    
+    private struct Constants {
+        static let inset: CGFloat = 5
+        struct FontSize {
+            static let largest: CGFloat = 200
+            static let smallest: CGFloat = 10
+            static let scaleFactor = smallest / largest
+        }
+        struct Pie {
+            static let opacity: CGFloat = 0.5
+            static let inset: CGFloat = 5
+        }
+    }
+}
+
+extension Animation {
+    static func spin(duration: TimeInterval) -> Animation {
+        .linear(duration: 1).repeatForever(autoreverses: false)
+    }
+}
+
+
+#Preview {
+    typealias Card = CardView.Card
+    return VStack {
+        HStack {
+            CardView(Card(isFaceUp: true, content: "X", id: "test1"))
+                .aspectRatio(4/3, contentMode: .fit)
+            CardView(Card(content: "X", id: "test1"))
+        }
+        HStack {
+            CardView(Card(isFaceUp: true, isMatched: true, content: "X", id: "test1"))
+            CardView(Card(isMatched: true, content: "X", id: "test1"))
+        }
+    }
+    .padding()
+    .foregroundColor(.green)
+}
+
---
+++ Memorize/ViewModel/EmojiMemoryGame.swift
@@ -0,0 +1,48 @@
+//
+//  EmojiMemoryGame.swift
+//  Memorize
+//
+//  Created by Sami Alhamad on 9/10/24.
+//
+
+import SwiftUI
+
+class EmojiMemoryGame: ObservableObject {
+    typealias Card = MemoryGame<String>.Card
+    
+    private static let emojis = ["👻", "🎃", "🕷️", "😈", "💀", "🕸️", "🧙", "🙀", "👹", "😱", "☠️", "🍭"]
+        
+    private static func createMemoryGame() -> MemoryGame<String> {
+        return  MemoryGame(numberOfPairsOfCards: 9) { pairIndex in
+            if emojis.indices.contains(pairIndex) {
+                return emojis[pairIndex]
+            } else {
+                return "⁉️"
+            }
+        }
+    }
+        
+    @Published private var model = createMemoryGame()
+    
+    var cards: Array<Card> {
+        model.cards
+    }
+    
+    var color: Color {
+        .orange
+    }
+    
+    var score: Int {
+        model.score
+    }
+    
+    // MARK: - Intents
+    
+    func shuffle() {
+        model.shuffle()
+    }
+    
+    func choose(_ card: Card) {
+        model.choose(card)
+    }
+}
